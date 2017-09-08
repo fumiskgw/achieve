eb69186fd2b09bf57b27268ce1f15d9ba3726639
Fumi Sakegawa <fumi-i@nttpc.co.jp>
2017-09-08 08:36:09 +0000

    DIVE09課題

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
   
   def index
     @blogs = Blog.all
@@ -20,10 +20,11 @@ class BlogsController < ApplicationController
   
   def create
     @blog = Blog.new(blogs_params)
+    @blog.user_id = current_user.id
     if @blog.save
-    redirect_to blogs_path, notice: "ブログを作成しました！"
+     redirect_to blogs_path, notice: "ブログを作成しました！"
     else
-    render 'new'
+     render 'new'
     end
   end
   
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times do |n|
+  title = Faker::Pokemon.name
+  content = Faker::Pokemon.location
+  Blog.create!(title: title,
+               content: content,
+               user_id: 1
+               )
+end
