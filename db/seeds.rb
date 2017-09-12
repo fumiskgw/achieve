00b450c48672d428322f5edbe789240fcd61f21a
Fumi Sakegawa <fumi-i@nttpc.co.jp>
2017-09-12 03:28:42 +0000

    DIVE09課題SEED

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 100.times do |n|
-  title = Faker::Pokemon.name
-  content = Faker::Pokemon.location
-  Blog.create!(title: title,
-               content: content,
-               user_id: n
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
                )
-               n = n + 1
 end
